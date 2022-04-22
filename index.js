a72df8496e369f0f6f9cf59624d5cfc82e3dc51d
clare-cs <clarechan.cs@gmail.com>
2022-04-23 00:07:27 +0800

    make it better

--- index.js
+++ index.js
@@ -92,6 +92,6 @@ app.get('/info', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
