39ed213ec55c0a9f26e794d2188245addc344394
clare-cs <clarechan.cs@gmail.com>
2022-04-22 15:34:55 +0800

    3.4 & fix 3.2

--- index.js
+++ index.js
@@ -43,7 +43,14 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
-app.get('/api/info', (req, res) => {
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p>${new Date()}<p></p>`)
 })
 
